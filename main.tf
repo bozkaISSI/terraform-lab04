e603f20256903e060a80b1ee9659a043d204f6f3
zewazla <zewazla@gmail.com>
2025-06-24 00:16:29 +0200

    feat: terraform bucket

--- main.tf
+++ main.tf
@@ -11,3 +11,19 @@ provider "github" {
   token = var.github_token
   owner = "bozkaISSI"
 }
+provider "aws" {
+  region = "eu-central-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-bozka123"
+    key            = "lab04/github/terraform.tfstate"
+    region         = "eu-central-1"
+    encrypt        = true
+    dynamodb_table = "terraform-locks"
+  }
+}
+
+
+
---
+++ state_backend.tf
@@ -0,0 +1,32 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-state-bozka123"
+
+  tags = {
+    Name        = "Terraform Remote State"
+    Environment = "dev"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "Terraform Locks Table"
+    Environment = "dev"
+  }
+}
